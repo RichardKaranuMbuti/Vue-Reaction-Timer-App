798764afd2e48c09820f72a5178aac1a371f0d33
RichardKaranuMbuti <officialforrichardk@gmail.com>
2023-05-07 07:13:44 +0300

    created two compnents - Block and Results

---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+   <p>Reaction time - {{ score }} ms </p>  
+   <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if (this.score < 250){
+            this.rank="Great Reaction Time"
+        }
+        else if (this.score <400){
+            this.rank="Rapid Reflexes"
+        }
+        else{
+            this.rank=" Snail Pace"
+        }
+    }
+
+}
+</script>
+
+<style>
+.rank {
+    font-size: 1.4 em;
+    color: aqua;
+    font-weight: bold;
+}
+
+</style>
